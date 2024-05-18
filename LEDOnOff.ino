62f7e03b9ea5d57c465a064ee1fccee03696aa87
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-18 21:51:44 +0530

    Add files via upload

---
+++ LEDOnOff.ino
@@ -0,0 +1,16 @@
+int led = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,LOW); //LED is off by default
+  Serial.println("LED is off");
+  delay(1000);//Off for one second
+  digitalWrite("LED is On");
+  delay(3000);//LED is On for 3 seconsds
+}
