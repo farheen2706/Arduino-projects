0b5b75659e899569760f5f5bb40caa8a61f74022
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-18 21:31:06 +0530

    Add files via upload

---
+++ Infrared.ino
@@ -0,0 +1,25 @@
+int led=13,ir=12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  pinMode(ir,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int data=digitalRead(ir);
+  if(data == HIGH)
+  {
+    Serial.println("Bug");
+    digitalWrite(led,HIGH);
+  }
+  else
+  {
+    Serial.println("Bug Found");
+    digitalWrite(led,LOW);
+  }
+  
+  // put your main code here, to run repeatedly:
+
+}
