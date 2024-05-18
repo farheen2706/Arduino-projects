cc6aaebc0827350ac483961b83a258f9c2338321
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-18 21:44:29 +0530

    Update and rename LEDlight.ino to ServoMotor.ino

--- ServoMotor.ino
+++ ServoMotor.ino
@@ -1,5 +1,5 @@
 //Global variable declaration
-#include<Servo.h>
+#include<Servo.h> //importing package for servo motor
 
 Servo s;
 int led = 8;
@@ -7,12 +7,12 @@ void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 s.attach (9);
-//pinMode(led,OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//digitalWrite(led,LOW);
+//Servo motor rotation
 for(int i=0;i<=180;i++)
 {
   s.write(i);
@@ -25,21 +25,4 @@ for(int i=180;i>=0;i--)
   delay(10);
   Serial.println("Rotation Anticlockwise");
 }
-/*for(int i=0; i<=255; i++)
-{
-  analogWrite(led,HIGH);
-  delay(50);
-  Serial.println("Fade in");
-}
-for(int i=255; i>=0; i--)
-{
-  analogWrite(led,LOW);
-  delay(50);
-  Serial.println("Fade out");
-}*/
-//Serial.println("LED is Off");
-//delay(1000);
-//digitalWrite(led, HIGH);
-//Serial.println("LED is On");
-//delay(3000);
 }
