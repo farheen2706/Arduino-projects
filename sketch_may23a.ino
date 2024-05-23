c2918c0b0b3c563e412675a73a45d3d11506507d
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-23 12:33:40 +0530

    Add files via upload

---
+++ sketch_may23a.ino
@@ -0,0 +1,21 @@
+#include <RH_ASK.h>
+#include <SPI.h>//serial communication
+//creating amplitude shift keyung
+RH_ASK rf_driver;//ask object to access the functions related to the readihead library
+
+void setup() {
+  // put your setup code here, to run once:
+  if (!rf_driver.init())
+  {
+    Serial.print("Failed");//initializing the ASK object
+  
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  const char *msg = "Transmitting"; //MEssage for transmitting
+  rf_driver.send((uint8_t *)msg, strlen(msg));//takes two parameters array of data(encoded using uint_t) and number of bytes(length of the string)
+  rf_driver.waitPacketSent();//waits until the transmission is complete
+  delay(1000);
+}
