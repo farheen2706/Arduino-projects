e9bdeb384748225bac6029ad0b211f8228de8b67
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-08-29 12:11:24 +0530

    Update LEDOnOff.ino

--- LEDOnOff.ino
+++ LEDOnOff.ino
@@ -9,7 +9,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led,LOW); //LED is off by default
-  Serial.println("LED is off");
+  Serial.println("LED is off");//LED should be off
   delay(1000);//Off for one second
   digitalWrite("LED is On");
   delay(3000);//LED is On for 3 seconsds
