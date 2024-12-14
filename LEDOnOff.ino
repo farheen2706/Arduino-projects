9517f8f7068500085d781c0197723c426b49abdc
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-12-14 21:41:54 +0530

    Update LEDOnOff.ino

--- LEDOnOff.ino
+++ LEDOnOff.ino
@@ -12,5 +12,5 @@ void loop() {
   Serial.println("LED is off");//LED should be off
   delay(1000);//Off for one second
   digitalWrite("LED is On");
-  delay(3000);//LED is On for 3 seconsds
+  delay(3000);//LED is On for 3 seconds
 }
